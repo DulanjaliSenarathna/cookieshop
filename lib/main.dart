f1a10085c485110b0a7f19ce55c07c0054c913d3
Dulanjali-Senarathna <dishanisenarathna96@gmail.com>
2021-07-22 18:02:15 +0530

    Bottom bar done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cookieshop/bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,7 +21,14 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,8 +57,63 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: ListView(
-        
+        padding: EdgeInsets.all(20.0),
+        children: <Widget>[
+          SizedBox(height: 15.0,),
+          Text('Categories',
+          style: TextStyle(
+            fontFamily: 'Varela',
+            fontSize: 42.0,
+            fontWeight: FontWeight.bold
+          ),
+          ),
+          SizedBox(height: 15.0,),
+          TabBar(
+            controller: _tabController,
+            indicatorColor: Colors.transparent,
+            labelColor: Color(0xFFC88D67),
+            isScrollable: true,
+            labelPadding: EdgeInsets.only(right: 45.0),
+            unselectedLabelColor: Color(0xFFCDCDCD),
+            tabs: [
+              Tab(
+                child: Text(
+                  "Cookies",
+                  style: TextStyle(
+                    fontFamily: 'Varela',
+                    fontSize: 21.0
+                  ),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  "Cakes",
+                  style: TextStyle(
+                      fontFamily: 'Varela',
+                      fontSize: 21.0
+                  ),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  "Ice Creams",
+                  style: TextStyle(
+                      fontFamily: 'Varela',
+                      fontSize: 21.0
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Color(0xFFF17532),
+        child: Icon(Icons.fastfood),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomBar(),
     );
   }
 }
